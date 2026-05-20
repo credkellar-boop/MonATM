6fdcb1ee632155a41692ae6830774446f51bb834
Darion Kellar <credkellar@gmail.com>
2026-05-20 02:56:20 -0700

    Add display_menu method to TxSelectState

--- src/states/tx_select.rs
+++ src/states/tx_select.rs
@@ -1,1 +1,13 @@
 pub struct TxSelectState;
+
+impl TxSelectState {
+    pub fn display_menu() {
+        println!("\n--- MonATM MAIN MENU ---");
+        println!("1. Cash Withdrawal");
+        println!("2. Fiat Deposit");
+        println!("3. Balance Inquiry");
+        println!("4. Crypto Out-Ramp (Cash-out Crypto)");
+        println!("5. Cancel & Eject Card");
+        println!("------------------------");
+    }
+}
