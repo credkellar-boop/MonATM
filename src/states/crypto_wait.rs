43045ca06abe570010ca5b6de6ca27b7e53c0c12
Darion Kellar <credkellar@gmail.com>
2026-05-20 02:57:39 -0700

    Add CryptoWaitState struct with methods

--- src/states/crypto_wait.rs
+++ src/states/crypto_wait.rs
@@ -1,1 +1,17 @@
-pub struct CryptoWaitState;
+pub struct CryptoWaitState {
+    pub target_confirmations: u8,
+}
+
+impl CryptoWaitState {
+    pub fn new(confirmations: u8) -> Self {
+        Self { target_confirmations: confirmations }
+    }
+
+    pub fn display_status(&self, current: u8) {
+        println!(
+            "[UI Screen] Awaiting Settlement: [{}/{}] confirmations detected in mempool...", 
+            current, 
+            self.target_confirmations
+        );
+    }
+}
