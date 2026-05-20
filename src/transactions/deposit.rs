6f95c333b9c83b44247c53107cff1a089b8ab55c
Darion Kellar <credkellar@gmail.com>
2026-05-20 02:59:49 -0700

    Add constructor for FiatDepositTransaction

--- src/transactions/deposit.rs
+++ src/transactions/deposit.rs
@@ -5,6 +5,10 @@ pub struct FiatDepositTransaction {
 }
 
 impl FiatDepositTransaction {
+    pub fn new(account_id: &str) -> Self {
+        Self { target_account_id: account_id.to_string() }
+    }
+
     pub fn process_deposit(&self, vault: &mut DepositVault, bills: u32, denomination: u32) {
         let credited_amount = vault.accept_cash_intake(bills, denomination);
         println!("[Transaction Engine] Adjusting account balance for account {} up by +${}", self.target_account_id, credited_amount);
